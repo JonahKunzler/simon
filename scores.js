d5172ddc067bbed3e29422b3a263348a6c0c7e0c
JonahKunzler <jonahk12@gmail.com>
2023-03-06 23:10:25 -0700

    Fixing issue

--- scores.js
+++ scores.js
@@ -2,7 +2,7 @@ function loadScores() {
   let scores = [];
   const scoresText = localStorage.getItem('scores');
   if (scoresText) {
-    scores = JSON.parse(scoresText)
+    scores = JSON.parse(scoresText);
   }
 
   const tableBodyEl = document.querySelector('#scores');
